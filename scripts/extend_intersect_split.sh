e6f41cba425ae45ab66a66e5a3f8b1d5530da9b5
Chris Clarkson <cc16956@essex.ac.uk>
2019-04-08 14:09:52 +0100

    Update extend_intersect_split.sh

--- scripts/extend_intersect_split.sh
+++ scripts/extend_intersect_split.sh
@@ -18,6 +18,6 @@ i=${pair[1]}
 
 bedtools slop -l -100 -r 2000 -i quintile_${i}_${TF}_sites.bed -g /storage/projects/teif/mm9_generic_data/mm9.genome > quintile_${i}_${TF}_sites_extended.bed
 sortBed -i quintile_${i}_${TF}_sites_extended.bed > quintile_${i}_${TF}_sites_extended_sorted.bed
-intersectBed -a /storage/projects/teif/mESC/nucleosome_positioning_datasets/chemical_mapping_mnase/chemical_mnase_formatted_sorted_Chemical_mapping_MNase_WT.bed -b quintile_${i}_${TF}_sites_extended_sorted.bed -u > mnase_intersect_quintile_${i}_${TF}_sites_extended_sorted.bed
+intersectBed -a ../data/MNAse.bed -b quintile_${i}_${TF}_sites_extended_sorted.bed -u > mnase_intersect_quintile_${i}_${TF}_sites_extended_sorted.bed
 perl /storage/projects/teif/nuctools.3.0/extract_chr_bed.pl --output=${TF}_${i} --input=mnase_intersect_quintile_${i}_${TF}_sites_extended_sorted.bed -p all
 
