5d46ee7c40e968a9312134c8895871ad0daa2a2c
Chris Clarkson <cc16956@essex.ac.uk>
2019-04-08 14:42:01 +0100

    Add files via upload

---
+++ scripts/step_by_step.sh
@@ -0,0 +1,8 @@
+qsub download_and_map_MNase.sh #wait for this to finish
+qrsh
+Rscript TF_binding_site_affinity.R
+
+Rscript quantiles.R CTCF 6 #specify TF to split quantiles and column to use for affinity metric
+exit
+
+qsub -t 1-45 -tc 10 
