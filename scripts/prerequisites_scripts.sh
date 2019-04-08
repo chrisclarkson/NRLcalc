32b4b1fa3869d028c3dd175a77a1341a87946f35
Chris Clarkson <cc16956@essex.ac.uk>
2019-04-08 17:06:41 +0100

    Create prerequisites_scripts.sh

---
+++ scripts/prerequisites_scripts.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/arq5x/bedtools2 #bedtools
+git https://github.com/homeveg/nuctools #nuctools
+
+R
+BiocManager::install("GenomicRanges")
+BiocManager::install("AnnotationHub")
+BiocManager::install("TFBSTools")
+BiocManager::install("JASPAR2018")
+BiocManager::install("TFBSTools")
+BiocManager::install("BSgenome.Mmusculus.UCSC.mm9") 
+install.packages("devtools")
+devtools::install_github("matthuska/tRap")
