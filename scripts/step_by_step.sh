e2afcdab6b446c78e23e1f1cace3192f4b56e3af
Chris Clarkson <cc16956@essex.ac.uk>
2020-01-23 10:59:46 +0000

    Update step_by_step.sh

--- scripts/step_by_step.sh
+++ scripts/step_by_step.sh
@@ -5,4 +5,4 @@ Rscript TF_binding_site_affinity.R
 Rscript quantiles.R CTCF 6 #specify TF to split quantiles and column to use for affinity metric
 exit
 
-qsub -t 1-45 -tc 10 
+qsub -t 1-45 -tc 10 NRL_all_combinations.sh
